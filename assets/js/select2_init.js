ab73b123577799cd7ffa57084a0feae6fcda4a9f
Ryan Williams <rwilliams@mla.org>
2017-09-12 16:41:21 -0400

    hide exact match from select2 results & autoselect case-insensitive matches

--- assets/js/select2_init.js
+++ assets/js/select2_init.js
@@ -13,38 +13,48 @@ jQuery(document).ready( function($) {
   $(".js-basic-multiple").select2({
   });
 
-  $(".js-basic-multiple-tags").select2({
+
+  $( '.js-basic-multiple-tags' ).select2( {
     minimumInputLength: 1,
-    tags: "true",
+    tags: true,
     tokenSeparators: [','],
     ajax: {
-      url: '/wp-json/mla-academic-interests/v1/terms'
+      url: '/wp-json/mla-academic-interests/v1/terms',
+      cache: true
+    },
+    templateResult: function( result ) {
+      // hide result which exactly matches user input to avoid confusion with differently-cased matches
+      if ( $('.select2-search__field').val() == result.text ) {
+        result.text = null;
+      }
+
+      return result.text;
     }
-  });
+  } );
 
   // ensure user-input terms conform to existing terms regardless of case
   // e.g. if user enters "music" and "Music" exists, select "Music"
-  $('.js-basic-multiple-tags').on('select2:selecting', function(e) {
-    var input_term = e.params.args.data.text;
-    var existing_terms = $('.select2-results__option').not('.select2-results__option--highlighted');
-    var selected_terms = $('.js-basic-multiple-tags').val() || [];
-    var Select2 = $('.js-basic-multiple-tags').data('select2');
+  $( '.js-basic-multiple-tags' ).on( 'select2:selecting', function( e ) {
+    var input_term = e.params.args.data.id;
+    var existing_terms = $( '.select2-results__option' ).not( '.select2-results__option--highlighted' );
+    var selected_terms = $( '.js-basic-multiple-tags' ).val() || [];
+    var Select2 = $( '.js-basic-multiple-tags' ).data( 'select2' );
 
-    $.each(existing_terms, function(i, term_el) {
-      var term = $(term_el).text();
+    $.each( existing_terms, function( i, term_el ) {
+      var term = $( term_el ).text();
 
       // if this term already exists with a different case, select that instead
-      if (input_term != term && input_term.toUpperCase() == term.toUpperCase()) {
+      if ( input_term.toUpperCase() == term.toUpperCase() ) {
         // overwrite the user-input term with the canonical one
         e.params.args.data.id = term;
         e.params.args.data.text = term;
 
         // trigger another select event with the updated term
-        Select2.constructor.__super__.trigger.call(Select2, 'select', e.params.args);
+        Select2.constructor.__super__.trigger.call( Select2, 'select', e.params.args );
 
         e.preventDefault();
       }
-    });
-  });
+    } );
+  } );
 
 } );
